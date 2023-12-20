fa9903472828ee79c276428eba9555bd62620c08
Soye LEE <solee2@made-f0Cr13s1.clusters.42paris.fr>
2023-12-20 15:29:04 +0100

    validate map norm done

--- srcs/validate_map.c
+++ srcs/validate_map.c
@@ -6,7 +6,7 @@
 /*   By: solee2 <solee2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 02:48:37 by solee2            #+#    #+#             */
-/*   Updated: 2023/12/20 15:15:14 by solee2           ###   ########.fr       */
+/*   Updated: 2023/12/20 15:28:35 by solee2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,10 +64,7 @@ int	map_content(t_vars *vars)
 		y++;
 	}
 	if (vars->players != 1)
-	{
-		/* free_map(vars->first_map, vars->height); */
 		return (1);
-	}
 	return (0);
 }
 
